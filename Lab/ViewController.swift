990f2e405ac11b8cac08a9675fbb130b6204bf03
Han Xi <ahanxi@126.com>
2017-09-26 21:00:38 +0800

    Human Seg 224*224 (TOO SLOW)

--- Lab/ViewController.swift
+++ Lab/ViewController.swift
@@ -16,35 +16,42 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         
-        let image = UIImage(named: "pic_hd.jpg")
-        
-        // gear the image to meet the request of the MobileNet
-        let width : CGFloat = 112.0
-        let height : CGFloat = 112.0
+        // supposed height ang width of the input image of the MobileNet
+        let width : CGFloat = 224.0
+        let height : CGFloat = 224.0
         let width_Int : Int = Int(width)
         let height_Int : Int = Int(height)
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-        image?.draw(in:CGRect(x: 0, y: 0, width: width, height: height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()/**/
         
-        //let newImage = resize_padding2(image: image!, dstshape: [width_Int, height_Int])
-        //MultiArray
-        //let newImage =
+        // a black imahe used to do the padding
+        let pada : MultiArray = MultiArray<Double>(shape : [3, height_Int, width_Int])
+        for i in 0 ..< 3 * width_Int * height_Int
+        {
+            pada.pointer[i] = 0.0
+        }
+        let pad = pada.image(offset: 0, scale: 1)
         
-        /*let uv = UIView(frame: CGRect(x: 50, y: 50, width: 400, height: 400))
+        // the original image to do HumanSeg
+        let image = UIImage(named: "pic_hd.jpg")
+        
+        // resize and padding
+        let newImage = resize_padding2(image: image!, dstshape: [height_Int, width_Int], pad : pad!)
+        
+        // show the image after resizing and padding
+        let uv = UIView(frame: CGRect(x: 50, y: 50, width: width_Int, height: height_Int))
         let color = UIColor(patternImage: newImage)
         uv.backgroundColor = color
-        self.view.addSubview(uv)*/
+        self.view.addSubview(uv)
         
-        //let input = pixelBufferFromImage(image: newImage!)!
-        let input = newImage?.pixelBuffer(width: Int(width), height: Int(height))
+        // convert the resized and padded image to CVImageBuffer( which is the input format for CoreML MobileNet )
+        let input = newImage.pixelBuffer(width: Int(width), height: Int(height))
 
         // MobileNet runs here
         if #available(iOS 11.0, *)
         {
+            // CoreML Net init
             let mobileNet = MobileNet()
             
+            // Net RUNS in prediction() and return the answer to output
             guard
                 let output = try? mobileNet.prediction(data: input!)
             else
@@ -52,11 +59,7 @@ class ViewController: UIViewController
                 fatalError("Unexpected Error.")
             }
             
-            //var pointer = UnsafeMutablePointer<Double>(OpaquePointer(output.prob.dataPointer))
-            // prob : Double 1 x 1 x 2 x 112 x 112 array
-            //               0   0   1    i     j
-            //pointer += MemoryLayout<Double>.stride * width_Int * height_Int
-
+            // turn the output MLMultiArray into a gray image
             let ma = MultiArray<Double>(output.prob)
             var mm = MultiArray<Double>(shape: [height_Int, width_Int], initial: 0.0)
             
@@ -70,175 +73,39 @@ class ViewController: UIViewController
             
             let outputImage : UIImage = mm.image(offset: 0, scale: 225)!
             
-            
-            /*let ci : CIImage = CIImage(bitmapData: Data(bytes: pointer, count: Int(width) * Int(height)),
-                                       bytesPerRow: MemoryLayout<Double>.stride * Int(width),
-                                       size: CGSize(width: width, height: height),
-                                       format: CIFormat(kCVPixelFormatType_OneComponent8),
-                                       colorSpace: (CGColorSpace.linearGray as! CGColorSpace))
-            let outputImage : UIImage = UIImage(ciImage: ci)*/
-            
-            //let d = NSData(bytesNoCopy: pointer, length: MemoryLayout<Double>.stride * Int(width) * Int(height))
-            //let e = Data(bytes: pointer, count: Int(width) * Int(height))
-            //print(pointer[index(row: i, col: j)])
-            
-            let uv = UIView(frame: CGRect(x: 60, y: 60, width: width_Int, height: height_Int))
+            // show the gray image on the screen
+            let uv = UIView(frame: CGRect(x: 50, y: 300, width: width_Int, height: height_Int))
             let color = UIColor(patternImage: outputImage)
             uv.backgroundColor = color
-            self.view.addSubview(uv)/**/
-        }/**/
+            self.view.addSubview(uv)
+        }
     }
     
-    func resize_padding2(image : UIImage, dstshape : [Int]) -> UIImage
+    func resize_padding2(image : UIImage, dstshape : [Int], pad : UIImage) -> UIImage
     {
-        let s = NSDate()
-        print("s@\(s)")
-        
+        // decide whether to shrink in height or width
         let height = image.size.height
         let width = image.size.width
         let ratio = width / height
         let dst_width = Int(min(CGFloat(dstshape[1]) * ratio, CGFloat(dstshape[0])))
         let dst_height = Int(min(CGFloat(dstshape[0]) / ratio, CGFloat(dstshape[1])))
-        let origin = [Int((dstshape[1] - dst_height) / 2), Int((dstshape[0] - dst_width) / 2)]
+        let origin = [Int((dstshape[0] - dst_height) / 2), Int((dstshape[1] - dst_width) / 2)]
         
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+        // a black background padding
+        UIGraphicsBeginImageContext(CGSize(width: dst_width, height: dst_height))
         image.draw(in:CGRect(x: 0, y: 0, width: dst_width, height: dst_height))
         let tmpImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-        let blackImage = UIImage(ciImage: CIImage(color: CIColor.black))
-        blackImage.draw(in:CGRect(x: 0, y: 0, width: width, height: height))
-        //tmpImage?.draw(at: CGPoint(x: origin[0], y: origin[1]))
+        // draw the resize image in the center
+        UIGraphicsBeginImageContext(CGSize(width: dstshape[1], height: dstshape[0]))
+        pad.draw(in:CGRect(x: 0, y: 0, width: dstshape[1], height: dstshape[0]))
+        tmpImage?.draw(at: CGPoint(x: origin[1], y: origin[0]))
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
-        print("e")
-        print("cost\(NSDate().timeIntervalSince(s as Date))")
         
         return newImage!
     }
-
-    
-    /*func runMobileNet()
-    {
-        let image = UIImage(named: "timg.jpeg")
-        
-        // gear the image to meet the request of the MobileNet
-        let width : CGFloat = 112.0
-        let height : CGFloat = 112.0
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-        image?.draw(in:CGRect(x: 0, y: 0, width: width, height: height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        let input = pixelBufferFromImage(image: newImage!)!
-        
-        // get the timestamp before the net runs
-        let startTimeStamp = NSDate()
-        print("MobileNet Starts Running @ \(startTimeStamp)")
-        
-        // MobileNet runs here
-        if #available(iOS 11.0, *)
-        {
-            let mobileNet = MobileNet()
-            
-            for _ in 0 ..< 1000
-            {
-                guard
-                    let _ = try? mobileNet.prediction(data: input)
-                    else
-                {
-                    fatalError("Unexpected Error.")
-                }
-                
-                //print("Prediction: " + output.classLabel)
-                //print(output.classLabel)
-                //print(output.classLabelProbs)
-            }
-        }
-        
-        // get the timestamp after the net runs
-        let endTimeStamp = NSDate()
-        print("MobileNet Ends Running @ \(endTimeStamp)")
-        print("MobileNet Runs for 1000 times, costing ")
-        print("\(endTimeStamp.timeIntervalSince(startTimeStamp as Date)) s")
-        print("Averange Time cost is ")
-        print("\(endTimeStamp.timeIntervalSince(startTimeStamp as Date) * 1) ms / per pic")
-    }*/
-    
-    /*func pixelBufferFromImage(image: UIImage) -> CVPixelBuffer?
-    {
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        
-        guard
-            (status == kCVReturnSuccess)
-        else
-        {
-            return nil
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        
-        let context = CGContext(data: pixelData,
-                                width: Int(image.size.width),
-                                height: Int(image.size.height),
-                                bitsPerComponent: 8,
-                                bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
-                                space: rgbColorSpace,
-                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-        
-        context?.translateBy(x: 0, y: image.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        
-        return pixelBuffer
-    }*/
-    
-    /*func pixelBufferFromImage(image: UIImage) -> CVPixelBuffer?
-     {
-     let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-     var pixelBuffer : CVPixelBuffer?
-     let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-     
-     guard
-     (status == kCVReturnSuccess)
-     else
-     {
-     return nil
-     }
-     
-     CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-     let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-     
-     let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-     
-     let context = CGContext(data: pixelData,
-     width: Int(image.size.width),
-     height: Int(image.size.height),
-     bitsPerComponent: 8,
-     bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
-     space: rgbColorSpace,
-     bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-     
-     context?.translateBy(x: 0, y: image.size.height)
-     context?.scaleBy(x: 1.0, y: -1.0)
-     
-     UIGraphicsPushContext(context!)
-     image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-     UIGraphicsPopContext()
-     CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-     
-     return pixelBuffer
-     }*/
 
     override func didReceiveMemoryWarning()
     {
--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # HumanSeg IOS APP
 
-Demo project applying the coreML code MobileNet in the lab.
+Demo project applying HumanSeg MobileNet in the CoreML frame in the lab.
 
 ## Things Done
 - Calculate the efficiency of this net
 - Make a demo to make this net run correctly and give proper predicition
 
 ## TODO
-- Recover the original size of the prediction picture
-- Do proper padding for the input picture with less time complexity
+- Make this app run faster (At present, it takes too much time outside the Net for resizing, padding, and formt conversion)
 - Read pictutures from the memory of the iphone rather than the project lib
 
 ## Input and Output
-- Input: A picture size 112*112
+- Input: A picture size 224*224
 - Output: The prediction humanseg picture (gray)
 
 ## Usage of .mmodel File
